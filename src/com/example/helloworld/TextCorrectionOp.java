9d67ff31eb1266c1069e5f9c40347da428407a71
Benjamin Gordon <ben@bxg.org>
2013-01-24 21:10:19 -0700

    * Allow chained corrections.

--- src/com/example/helloworld/TextCorrectionOp.java
+++ src/com/example/helloworld/TextCorrectionOp.java
@@ -1,9 +1,13 @@
 package com.example.helloworld;
 
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
+import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 public class TextCorrectionOp implements Runnable
@@ -25,7 +29,22 @@ public class TextCorrectionOp implements Runnable
 		if (!(part instanceof AbstractTextEditor) )
 			return;
 		ITextEditor editor = (ITextEditor)part;
-		if ( oldOp != null ) {
+		IDocumentProvider dp = editor.getDocumentProvider();
+		IDocument doc = dp.getDocument(editor.getEditorInput());
+		ITextSelection selection =
+				(ITextSelection) editor.getSelectionProvider().getSelection();
+
+		if ( oldOp != null ) 
+		{
+			// Erase the old selection first if needed
+			if ( selection.getLength() > 0 ) {
+				try {
+					doc.replace( selection.getOffset(), selection.getLength(), "" );
+				} catch (BadLocationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 			editor.selectAndReveal( oldOp.offset, oldOp.length );
 			System.out.println( "Selected " + oldOp );
 		} else {
