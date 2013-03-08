b3565d19576bf3b5829705c4388854741c697b48
Benjamin Gordon <ben@bxg.org>
2013-03-08 11:07:01 -0700

    * Recognize <= and >=

--- src/com/example/helloworld/SpeechListener.java
+++ src/com/example/helloworld/SpeechListener.java
@@ -104,7 +104,9 @@ public class SpeechListener implements Runnable, SLResultListener {
 		tmp = tmp.replaceAll( "\\beight\\b", "8" );
 		tmp = tmp.replaceAll( "\\bnine\\b", "9" );
 		tmp = tmp.replaceAll( "\\bten\\b", "10" );
+		tmp = tmp.replaceAll( "\\bless than or equal to\\b", "<=" );
 		tmp = tmp.replaceAll( "\\bless than\\b", "<" );
+		tmp = tmp.replaceAll( "\\bgreater than or equal to\\b", ">=" );
 		tmp = tmp.replaceAll( "\\bgreater than\\b", ">" );
 		tmp = tmp.replaceAll( "\\bnot equals\\b", "!=" );
 		tmp = tmp.replaceAll( "\\bequals\\b", "=" );
