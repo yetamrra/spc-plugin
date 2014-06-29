53f36200ae23018ad5fc919bf9d82c92c4cd4e72
Benjamin Gordon <ben@bxg.org>
2014-06-29 17:10:48 -0600

    * Added GPL declaration to all source files.

--- src/com/example/helloworld/NewGrammarDialogNodeBehavior.java
+++ src/com/example/helloworld/NewGrammarDialogNodeBehavior.java
@@ -1,5 +1,26 @@
 package com.example.helloworld;
 
+/*
+ * Copyright 2012-2014 Benjamin M. Gordon
+ * 
+ * This file is part of the spoken compiler Eclipse plugin.
+ *
+ * The spoken compiler Eclipse plugin is free software: 
+ * you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The spoken compiler Eclipse plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the spoken compiler Eclipse plugin.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.io.IOException;
 
 import edu.cmu.sphinx.jsgf.JSGFGrammarException;
